e6ce6d24a62f61b56e117dac443260cdfef4410b
Eric Ferreira <eric@ferreira.world>
2017-02-27 21:20:07 -0500

    Add table view
    This commit adds the tableview to the left window in the tab controller. It also fixes the gitignore.

--- Project/TheBooks/FirstViewController.swift
+++ Project/TheBooks/FirstViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+		
+		let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+		
+		let table = UITableView(frame: view.frame)
+		
+        table.layer.borderColor = UIColor.green.cgColor
+        table.layer.borderWidth = 10
+        table.contentInset = UIEdgeInsets(
+			top: 44 + statusBarHeight,
+			left: 0,
+			bottom: tabBarController?.tabBar.frame.height ?? 49,
+			right: 0
+		)
+		table.dataSource = self
+		table.delegate = self
+        
+        view.addSubview(table)
+		
+		let toolbar = UIToolbar()
+		
+		toolbar.frame = CGRect(
+			x: 0,
+			y: 0,
+			width: view.frame.width,
+			height: 44 + statusBarHeight
+		)
+		
+		view.addSubview(toolbar)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+	
+	let items = Array(repeating: 0, count: 30)
 }
 
+extension FirstViewController: UITableViewDelegate {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+}
+
+extension FirstViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell()
+		
+		cell.textLabel?.text = "Item: \(items[indexPath.row])"
+		
+        return cell
+    }
+	
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+}
